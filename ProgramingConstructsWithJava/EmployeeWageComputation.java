b165df67633647c71be381d8381ffbae1e98aa45
Arvind2698 <r.arvind007@gmail.com>
2020-06-11 17:13:12 +0530

    Employee present/absent check

--- ProgramingConstructsWithJava/EmployeeWageComputation.java
+++ ProgramingConstructsWithJava/EmployeeWageComputation.java
@@ -2,5 +2,11 @@ public class EmployeeWageComputation {
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Employee Wage Computation Program");
+	int EmployeeAttendance=(int)(Math.random()*2);
+        if(EmployeeAttendance == 0){
+            System.out.println("Employee is absent");
+        }else{
+            System.out.println("Employee is present");
+        }
     }
 }
