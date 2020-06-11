8c1477ef3108ed80a9cdbfc04a297f0936eaecf6
Arvind2698 <r.arvind007@gmail.com>
2020-06-11 17:46:43 +0530

    Part-time employee wages

--- ProgramingConstructsWithJava/EmployeeWageComputation.java
+++ ProgramingConstructsWithJava/EmployeeWageComputation.java
@@ -2,17 +2,34 @@ public class EmployeeWageComputation {
 
     public static int WagePerHour=20;
     public static int HoursPerDay=8;
+    public static int PartTimeHours=4;
     public static void main(String[] args) {
         System.out.println("Welcome to the Employee Wage Calculation Program");
         int WagePerDay;
-        int EmployeeAttendance=(int)(Math.random()*2);
+        int EmployeeAttendance=(int)(Math.random()*3);
         if(EmployeeAttendance == 0){
             WagePerDay=0;
             System.out.println("Employee is absent. Salary for the day: "+WagePerDay);
-        }else{
+        }else if(EmployeeAttendance ==1 ){
             WagePerDay=WagePerHour*HoursPerDay;
             System.out.println("Employee is present. Salary for the day: "+WagePerDay);
+        }else if(EmployeeAttendance ==2 ){
+            WagePerDay=WagePerHour*PartTimeHours;
+            System.out.println("Employee is present and working part-time. Salary for the day: "+WagePerDay);
         }
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
