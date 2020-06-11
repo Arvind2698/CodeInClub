b78b07211c3442fdc18c1c1687a3850a1a123d9f
Arvind2698 <r.arvind007@gmail.com>
2020-06-11 17:22:18 +0530

    Basic Salary calculation

--- ProgramingConstructsWithJava/EmployeeWageComputation.java
+++ ProgramingConstructsWithJava/EmployeeWageComputation.java
@@ -1,12 +1,18 @@
 public class EmployeeWageComputation {
 
+    public static int WagePerHour=20;
+    public static int HoursPerDay=8;
     public static void main(String[] args) {
-        System.out.println("Welcome to the Employee Wage Computation Program");
-	int EmployeeAttendance=(int)(Math.random()*2);
+        System.out.println("Welcome to the Employee Wage Calculation Program");
+        int WagePerDay;
+        int EmployeeAttendance=(int)(Math.random()*2);
         if(EmployeeAttendance == 0){
-            System.out.println("Employee is absent");
+            WagePerDay=0;
+            System.out.println("Employee is absent. Salary for the day: "+WagePerDay);
         }else{
-            System.out.println("Employee is present");
+            WagePerDay=WagePerHour*HoursPerDay;
+            System.out.println("Employee is present. Salary for the day: "+WagePerDay);
         }
     }
+
 }
